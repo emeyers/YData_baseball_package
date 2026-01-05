4846f3305b85c756c51d28ed191aeeaa5b9f1e8d
Ethan Meyers <ethan.meyeres@gmail.com>
2026-01-04 20:58:09 -0800

    Changed the default for whether files should be saved in directories

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='YData_baseball',
-    version='2026.6',
+    version='2026.7',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package to download YData Baseball files',
